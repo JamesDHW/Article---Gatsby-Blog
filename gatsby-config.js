fc948f90fbf46da85a9862f28e2e67282740cc6f
James Haworth Wheatman <JamesHaworthWheatman@gmail.com>
2021-07-25 11:23:57 +0100

    update readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://jamesdhw.github.io/blog/",
     title: "blog",
   },
   plugins: ["gatsby-plugin-styled-components"],
