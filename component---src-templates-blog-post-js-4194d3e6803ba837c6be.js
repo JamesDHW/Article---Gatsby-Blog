5a9e873cc82dfb790006aa179f6ac26810cbc64a
James Haworth Wheatman <JamesHaworthWheatman@gmail.com>
2021-07-31 22:55:04 +0100

    Updates

---
+++ component---src-templates-blog-post-js-4194d3e6803ba837c6be.js
@@ -0,0 +1,2 @@
+(self.webpackChunkblog=self.webpackChunkblog||[]).push([[989],{3546:function(e,t,l){"use strict";l.r(t);var n=l(7294),a=l(6246),r=l(906);t.default=function(e){var t=e.data.markdownRemark,l=t.html,u=t.frontmatter,o=u.title,c=u.thumbnail;return console.log(c),n.createElement(a.I,null,n.createElement(r.H1,null,o," 🚀"),n.createElement("div",{dangerouslySetInnerHTML:{__html:l}}))}}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-4194d3e6803ba837c6be.js.map
