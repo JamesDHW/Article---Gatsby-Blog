368a335c05a58d3e85c06e0e8839d72924e3bbeb
James Haworth Wheatman <JamesHaworthWheatman@gmail.com>
2021-08-15 19:12:05 +0100

    graphQL query for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,65 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import { SiteLayout } from '../components/SiteLayout/SiteLayout'
 import { BlogPreviewCard } from '../components/BlogPreviewCard/BlogPreviewCard'
 import { Title, Line } from '../stylesheet'
 
 import './index.css'
 
-const IndexPage = () => {
-  return (
-    <SiteLayout>
-      <Title>Articles</Title>
-      <Line />
-      {/* <BlogPreviewCard /> will go here */}
-    </SiteLayout>
-  )
-}
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges }
+  }
+}) => (
+  <SiteLayout>
+    <Title>Articles</Title>
+    <Line />
+    {edges.map(
+      ({
+        node: {
+          frontmatter: { slug, title, author, date, thumbnail_alt, thumbnail },
+          timeToRead
+        }
+      }) => (
+        <BlogPreviewCard
+          slug={slug}
+          title={title}
+          author={author}
+          thumbnail={thumbnail}
+          thumbnail_alt={thumbnail_alt}
+          minsToRead={timeToRead}
+          datePublished={date}
+        />
+      )
+    )}
+  </SiteLayout>
+)
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          timeToRead
+          frontmatter {
+            slug
+            title
+            date
+            author
+            thumbnail_alt
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 800
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
